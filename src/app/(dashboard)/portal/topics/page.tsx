e777ff4642532300868cf176f36926ee15161b47
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-07-19 10:25:55 +0300

    refactor: remove portal and AI modules

--- src/app/(dashboard)/portal/topics/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TopicList } from "@/components/portal/topic-list";
-
-export default function TopicsPage() {
-  return <TopicList />;
-}
--- src/app/(dashboard)/portal/trash/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { MessageList } from "@/components/portal/message-list";
-
-export default function TrashPage() {
-  return <MessageList folder="trash" />;
-}
