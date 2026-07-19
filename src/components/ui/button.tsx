759c263f08c07718f74db4f0bd4a9078e0411dba
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-07-19 20:55:29 +0300

    Экспортировать buttonVariants из @qt/outline-ui

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,2 +1,7 @@
-export { Button } from "@qt/outline-ui";
-export type { ButtonProps, ButtonSize, ButtonVariant } from "@qt/outline-ui";
+export { Button, buttonVariants } from "@qt/outline-ui";
+export type {
+  ButtonProps,
+  ButtonSize,
+  ButtonVariant,
+  ButtonVariantOptions,
+} from "@qt/outline-ui";
